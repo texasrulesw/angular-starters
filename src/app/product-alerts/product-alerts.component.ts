77e49b91a7e571e8006fa0a3690a402784806d9c
texasrulesw <texasrulesw@yahoo.com>
2020-02-14 12:39:44 -0600

    1 file modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import ( Input ) from '@angular/core';  //to receive a product as input//
+import (Input) from '@angular/core';  //to receive a product as input//
 
 @Component({
   selector: 'app-product-alerts',  //name of the component//
@@ -7,7 +7,8 @@ import ( Input ) from '@angular/core';  //to receive a product as input//
   styleUrls: ['./product-alerts.component.css'] //references CSS page//
 })
 export class ProductAlertsComponent implements OnInit {  //This export class handles the functionality of the component//
-
+  
+  @Input() product;  //@Input indicates the property value passes in from the components parent, the product list component//
 
   constructor() { }
 
