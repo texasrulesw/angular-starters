63cb4f41ef4fcbd2d9cc1c6049a50edbf0884ba4
texasrulesw <texasrulesw@yahoo.com>
2020-02-14 15:36:12 -0600

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';  
 import {products} from '../products';
 
 @Component({
@@ -16,6 +16,9 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params =>{
+      this.product=products[+params.get('productId')];
+    });
   }
 
 }
