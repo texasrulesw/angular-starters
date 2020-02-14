4e0cb779232c2085dd7f70a529c4dddc76718039
texasrulesw <texasrulesw@yahoo.com>
2020-02-14 15:56:22 -0600

    3 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,7 +17,7 @@ export class ProductDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.route.paramMap.subscribe(params =>{
-      this.product=products[+params.get('productId')];
+      this.product = products[+params.get('productId')];
     });
   }
 
