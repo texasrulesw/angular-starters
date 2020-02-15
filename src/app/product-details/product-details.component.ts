8fde2a8cdb4bd926153c6b90171feccc8053b5fd
texasrulesw <texasrulesw@yahoo.com>
2020-02-15 09:15:16 -0600

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -23,4 +23,9 @@ export class ProductDetailsComponent implements OnInit {
     });
   }
 
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
 }
